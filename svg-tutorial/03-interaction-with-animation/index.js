a9c53bb51b6a4d97df89c3421c04847bd59b016c
sckroll <kimsc0714@gmail.com>
2023-02-20 10:52:04 +0900

    CSS에서 JS 동적 패스 형식으로 애니메이션 변경

--- svg-tutorial/03-interaction-with-animation/index.js
+++ svg-tutorial/03-interaction-with-animation/index.js
@@ -1,3 +1,20 @@
-const $cirleSample = document.querySelector('.circle-sample')
+const $circleOuter1 = document.querySelector('.circle-outer-1')
+let startTime = null, animationId = null
 
-console.log($cirleSample.getTotalLength())
+$circleOuter1.setAttribute('stroke-dasharray', $circleOuter1.getTotalLength())
+
+const draw = timestamp => {
+  if (!startTime) startTime = timestamp
+  
+  const diffTime = timestamp - startTime
+  const circleLength = $circleOuter1.getTotalLength() - diffTime / 10
+  $circleOuter1.setAttribute('stroke-dashoffset', circleLength)
+
+  if (circleLength >= 0) {
+    animationId = requestAnimationFrame(draw)
+  } else {
+    cancelAnimationFrame(animationId)
+  }
+}
+
+animationId = requestAnimationFrame(draw)
