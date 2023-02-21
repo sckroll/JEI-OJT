77602ec0fbfdc1db93f89dd9ed50f1a73c0dea98
sckroll <kimsc0714@gmail.com>
2023-02-21 18:01:24 +0900

    선 굵기 및 색상 변경 기능 구현

--- svg-tutorial/04-drawable-pad/index.js
+++ svg-tutorial/04-drawable-pad/index.js
@@ -1,10 +1,11 @@
 const SVG_NS = 'http://www.w3.org/2000/svg'
 
-let isDragging = false, $currPath = null
+let isDragging = false, $currPath = null, pathColor = 'black', pathWidth = '2'
 
 const $memoContainer = document.getElementById('memo-container')
 const $btnUndo = document.querySelector('.btn-undo')
 const $btnClear = document.querySelector('.btn-clear')
+const $btnGroup = document.querySelector('.btn-group')
 
 /**
  * SVG 좌표계로 변환
@@ -28,10 +29,14 @@ window.addEventListener('pointerdown', ({ clientX, clientY, target }) => {
   $currPath = document.createElementNS(SVG_NS, 'path')
   $currPath.setAttributeNS(null, 'class', 'path-line drag-safe')
   $currPath.setAttributeNS(null, 'd', `M${x},${y}`)
+  $currPath.setAttributeNS(null, 'stroke', pathColor)
+  $currPath.setAttributeNS(null, 'stroke-width', pathWidth)
   $memoContainer.appendChild($currPath)
 
 })
-window.addEventListener('pointerup', () => {
+window.addEventListener('pointerup', ({ target }) => {
+  // 버튼 위에서 드래그할 경우 버튼이 눌리지 않는 현상 방지
+  if (!target.classList.contains('drag-safe')) return
   if (isDragging) isDragging = false
 
   // 점만 찍고 이동하지 않은 경우 해당 패스 삭제
@@ -55,16 +60,20 @@ window.addEventListener('pointerleave', () => {
 /**
  * 버튼 핸들링
  */
-$btnUndo.addEventListener('click', e => {
+$btnGroup.addEventListener('click', e => {
   e.stopPropagation()
 
-  if ($memoContainer.childElementCount === 0) return
-  $memoContainer.removeChild($memoContainer.lastChild)
-})
-$btnClear.addEventListener('click', e => {
-  e.stopPropagation()
-
-  while ($memoContainer.childElementCount > 0) {
+  const btnType = e.target.classList[0]
+  if (btnType === 'btn-undo') {
+    if ($memoContainer.childElementCount === 0) return
     $memoContainer.removeChild($memoContainer.lastChild)
+  } else if (btnType === 'btn-clear') {
+    while ($memoContainer.childElementCount > 0) {
+      $memoContainer.removeChild($memoContainer.lastChild)
+    }
+  } else if (btnType === 'btn-color') {
+    pathColor = e.target.classList[1] || 'black'
+  } else if (btnType === 'btn-width') {
+    pathWidth = e.target.classList[1] || '2'
   }
 })
