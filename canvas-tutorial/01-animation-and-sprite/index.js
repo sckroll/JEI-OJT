1175f04c632d2adc7dba5e305743cfd12853f325
sckroll <kimsc0714@gmail.com>
2023-02-28 08:38:23 +0900

    숫자 변경에 따른 캔버스 업데이트 및 가운데 정렬 적용

--- canvas-tutorial/01-animation-and-sprite/index.js
+++ canvas-tutorial/01-animation-and-sprite/index.js
@@ -1,9 +1,12 @@
 let first = 1, second = 1
 
+/**
+ * 폼 입력
+ */
+
 const $form = document.getElementById('form')
 $form.addEventListener('click', e => {
   e.preventDefault()
-  
 })
 
 const $first = document.getElementById('first')
@@ -12,16 +15,24 @@ $first.addEventListener('keyup', ({ target, key }) => {
   target.value = +key === 0 ? 1 : +key
   first = +key
 })
+$first.addEventListener('change', ({ target }) => {
+  first = +target.value
+})
+
 const $second = document.getElementById('second')
 $second.addEventListener('keyup', ({ target, key }) => {
   if (Number.isNaN(+key)) return
   target.value = +key === 0 ? 1 : +key
   second = +key
 })
+$second.addEventListener('change', ({ target }) => {
+  second = +target.value
+})
 
 const $submit = document.getElementById('submit')
 $submit.addEventListener('click', () => {
   console.log(`${first} + ${second}`);
+  draw()
 })
 
 const $canvas = document.getElementById('canvas')
@@ -61,19 +72,29 @@ const drawPath = (path, strokeColor = 'black', fillColor = 'transparent') => {
   ctx.fill(newPath)
 }
 
-for (let i = 0; i < first; i++) {
-  drawCircle(200 + i * 40, 160, 10, 'crimson')
-}
-for (let i = 0; i < second; i++) {
-  drawCircle(280 + 40 * (first - 1) + i * 40, 160, 10, 'slateblue')
-}
-drawPath(`M ${200} 140 h ${40 * (first - 1)} c 30 0 30 40 0 40 h -${40 * (first - 1)} c -30 0 -30 -40 0 -40 Z`)
-drawPath(`M ${280 + 40 * (first - 1)} 140 h ${40 * (second - 1)} c 30 0 30 40 0 40 h -${40 * (second - 1)} c -30 0 -30 -40 0 -40 Z`)
-drawPath(`M ${200 + 40 * (first - 1)} 140 c 0 -40 80 -40 80 0`)
+const draw = () => {
+  // 캔버스 지우기
+  ctx.clearRect(0, 0, $canvas.clientWidth, $canvas.clientHeight)
 
-ctx.fillStyle = 'black'
-ctx.font = '48px Arial'
-ctx.fillText(`${first} + ${second} = `, 285, 400)
-ctx.lineWidth = 3
-ctx.rect(455, 358, 50, 50)
-ctx.stroke()
+  let offsetX = 360 - (first + second - 2) * 20
+
+  for (let i = 0; i < first; i++) {
+    drawCircle(offsetX + i * 40, 160, 10, 'crimson')
+  }
+  for (let i = 0; i < second; i++) {
+    drawCircle(offsetX + 80 + 40 * (first - 1) + i * 40, 160, 10, 'slateblue')
+  }
+
+  ctx.lineWidth = 3
+  drawPath(`M ${offsetX} 140 h ${40 * (first - 1)} c 30 0 30 40 0 40 h -${40 * (first - 1)} c -30 0 -30 -40 0 -40 Z`, 'lightgrey')
+  drawPath(`M ${offsetX + 80 + 40 * (first - 1)} 140 h ${40 * (second - 1)} c 30 0 30 40 0 40 h -${40 * (second - 1)} c -30 0 -30 -40 0 -40 Z`, 'lightgrey')
+  drawPath(`M ${offsetX + 40 * (first - 1)} 140 c 0 -40 80 -40 80 0`, 'lightgrey')
+  
+  ctx.fillStyle = 'black'
+  ctx.font = '48px Arial'
+  ctx.fillText(`${first} + ${second} = `, 285, 400)
+  ctx.rect(455, 358, 50, 50)
+  ctx.stroke()
+}
+
+draw()
