e60d43b9a5e4ccd3f3ca182ccbec28a636a29b93
sckroll <kimsc0714@gmail.com>
2023-03-09 15:35:41 +0900

    일부 코드 분리 및 리팩토링

---
+++ canvas-tutorial/02-fabric-js/settings.js
@@ -0,0 +1,21 @@
+export const messages = {
+  TITLE_QUESTION_1: '삼각형을 찾아 고르세요.',
+  TITLE_QUESTION_2: '삼각형을 모두 찾아 고르세요.',
+  TITLE_MODAL_INCORRECT: '조금 아쉬워요!',
+  TITLE_MODAL_CORRECT: '와우! 잘했어요!',
+  MSG_MODAL_BUTTON: '다시 할래요!'
+}
+
+export const sources = {
+  SRC_CLICK_SOUND: './mp3/MP_Blop.mp3',
+  SRC_CORRECT_SOUND: './mp3/MP_스테이지 클리어 (레트로).mp3'
+}
+
+export const colors = ['red', 'royalblue', 'green', 'grey', 'magenta', 'gold', 'yellowgreen', 'skyblue']
+
+export const constants = {
+  CANVAS_MARGIN: 200,
+  TOTAL_LEVEL: 2,
+  MAX_INCORRECT_COUNT: 3,
+  SHAPE_COUNT: 8
+}
