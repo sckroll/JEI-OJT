588bd762fbe2dfe61ac40dc18ef7c7b9a768c0db
sckroll <kimsc0714@gmail.com>
2023-03-13 17:31:40 +0900

    로그인 폼 컴포넌트 분리

--- react-ojt/src/pages/SignIn/SignIn.tsx
+++ react-ojt/src/pages/SignIn/SignIn.tsx
@@ -1,40 +1,24 @@
-import { ChangeEvent, MouseEvent, ReactNode, useState } from "react"
+import { ReactNode } from "react"
 import JEILogo from "../../components/JEILogo"
-import InputBox from "../../components/InputBox"
-import Button from "../../components/Button"
+import LoginForm from "../../components/LoginForm"
 
 type PropTypes = {
   children: ReactNode
 }
 
-const FormContainer = ({ children }: PropTypes) => {
+const SignInContainer = ({ children }: PropTypes) => {
   return (
-    <form className="w-2/3 flex flex-col justify-center items-center p-4 gap-y-4">
+    <div className="h-screen flex flex-col justify-center items-center gap-y-16">
       { children }
-    </form>
+    </div>
   )
 }
 
 export default function SignIn() {
-  const [userInfo, setUserInfo] = useState({ id: '', password: '' })
-
-  const onChange = ({ target }: ChangeEvent) => {
-    if (target.id === 'user-id') setUserInfo({ ...userInfo, id: (target as HTMLInputElement).value })
-    if (target.id === 'user-pw') setUserInfo({ ...userInfo, password: (target as HTMLInputElement).value })
-  }
-  const onClick = (e: MouseEvent) => {
-    e.preventDefault()
-    console.log(e);
-  }
-
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-y-16">
+    <SignInContainer>
       <JEILogo />
-      <FormContainer>
-        <InputBox id="user-id" placeholder="아이디" onChange={onChange} />
-        <InputBox id="user-pw" type="password" placeholder="패스워드" onChange={onChange} />
-        <Button type="submit" onClick={onClick}>로그인</Button>
-      </FormContainer>
-    </div>
+      <LoginForm />
+    </SignInContainer>
   )
 }
